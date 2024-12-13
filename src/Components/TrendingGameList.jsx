957b0edbcc110cab8880cd89270cda499ad3213f
wacho91 <criskol.71@gmail.com>
2024-12-13 11:56:20 -0500

    TrendingGameList  Arquitecture omplete

--- src/Components/TrendingGameList.jsx
+++ src/Components/TrendingGameList.jsx
@@ -3,7 +3,22 @@
 const TrendingGameList = ({ gameList }) => {
   return (
     <div>
-      
+        <h2>Trending Games</h2>
+        <div>
+            {gameList.map(
+                (item, index) => 
+                    index < 4 && (
+                        <div key={item.id}>
+                            <img 
+                                src={item.background_image}
+                            />
+                            <div>
+                                <h2>{item.name}</h2>
+                            </div>
+                        </div>
+                    )
+            )}
+        </div>
     </div>
   )
 }
