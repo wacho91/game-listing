db831ceba6af45bb0b9d9cc47dcdce69399c9153
wacho91 <criskol.71@gmail.com>
2024-12-13 12:22:24 -0500

    Slider Component Compete

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -7,12 +7,15 @@ const Slider = ({ game }) => {
     },[])
 
   return (
-    <div>
-        <div>
-            <h2>{game.name}</h2>
-            <button>Get Now</button>
+    <div className="relative">
+        <div className="absolute bottom-0  bg-gradient-to-t w-full pb-10 from-slate-900 to-transparent p-5 rounded-xl">
+            <h2 className="text-[24px] text-white font-bold">{game.name}</h2>
+            <button className="bg-blue-700 text-white px-2 p-1">Get Now</button>
         </div>
-        <img src={game.background_image} alt="" />
+        <img 
+            src={game.background_image} 
+            className=" h-[170px] md:h-[320px] w-full object-cover rounded-xl"
+        />
     </div>
   )
 }
