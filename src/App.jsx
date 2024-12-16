fc1f7682e0658dfdb63d155808b9e4775d0ae474
wacho91 <criskol.71@gmail.com>
2024-12-16 12:20:40 -0500

    Changes in teh app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import Header from "./Components/Header"
 
+import { createContext, useEffect, useState } from "react"
+import Header from "./Components/Header"
+import { ThemeContext } from "./Context/ThemeContext"
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [theme, setTheme] = useState('light')
+
+  useEffect(() => {
+    setTheme(localStorage.getItem('theme')?localStorage.getItem('theme'): 'dark')
+  },[])
 
   return (
-    <div>
+    <ThemeContext.Provider value={{theme,setTheme}}>
+      <div className={`${theme} ${theme=='dark'?'bg-[#121212]':null}`}>
         <Header />
-    </div>
+      </div>
+    </ThemeContext.Provider>
   )
 }
 
