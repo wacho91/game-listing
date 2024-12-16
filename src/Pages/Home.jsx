8d193088c6e380ffcf68e579ee1d57090c4cce98
wacho91 <criskol.71@gmail.com>
2024-12-16 12:43:44 -0500

    Home Arquitecture Complete

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,44 @@
+import { useEffect, useState } from "react"
+
+import SideNavGenresList from "../Components/SideNavGenresList"
+import GlobalApi from "../Services/GlobalApi"
+import Slider from "../Components/Slider"
+import PopularGameList from "../Components/PopularGameList"
+import TrendingGameList from "../Components/TrendingGameList"
+
 
 const Home = () => {
+
+    const [ gameList, setGameList ] = useState([])
+    const [ gameListByGenre, setGameListByGenre ] = useState([])
+
+    useEffect(() => {
+        GlobalApi.getPopularGame.then(res => {
+            setGameList(res.data.results)
+        })
+        getGameList(4);
+    },[])
+
+    const getGameList = (genreId) => {
+        if(genreId != 0) {
+            GlobalApi.getGameListByGenreId(genreId).then(res => {
+                setGameListByGenre(res.data.results)
+            })
+        }
+    }
+
   return (
     <div>
-       Home Component
+       <div>
+           <SideNavGenresList selectedGenreId={(genreId) => getGameList(genreId)}/>
+       </div>
+       {gameList?.length&&gameListByGenre?.length>0 ?
+            <div>
+                <Slider />
+                <TrendingGameList />
+                <PopularGameList />
+            </div>
+        :null}
     </div>
   )
 }
