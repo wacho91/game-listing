893b6177d27e1168d164e2f48fe77db15b7b4734
wacho91 <criskol.71@gmail.com>
2024-12-16 11:47:28 -0500

    Heaer Arquitecture Complete

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,44 @@
 
-import { useState,useRef,useEffect } from 'react';
+import { useState,useEffect, useContext } from 'react';
 import Logo from '../assets/Images/logo.png';
 import Logo1 from '../assets/Images/logo1.png';
 
+import SideNavGenresList from './SideNavGenresList';
+import { HiMoon, HiOutlineBars3CenterLeft, HiOutlineMagnifyingGlass, HiOutlineXMark, HiSun } from 'react-icons/hi2';
+import { ThemeContext } from '../Context/ThemeContext';
+
 const Header = () => {
+
+  const [ toggle, setToggle ] = useState(false);
+  const {theme,setTheme}=useContext(ThemeContext)
+
+  useEffect(() => {
+    console.log("THEME--", theme);
+  },[]);
+
   return (
     <div>
-        Header Component
+        <img src={ Logo } alt="" />
+        <div>
+          {!toggle ? <HiOutlineBars3CenterLeft  onClick={() => setToggle(!toggle)} />
+          :<HiOutlineXMark onClick={() => setToggle(!toggle)} />}
+
+          {toggle ? 
+            <div>
+                <SideNavGenresList />
+            </div>: null
+          }
+        </div>
+        <div>
+          <HiOutlineMagnifyingGlass />
+          <input type="text" placeholder='Search Games' />
+        </div>
+        <div>
+            {
+              theme === "dark" ? <HiSun onClick={()=>setTheme('light')} />
+              : <HiMoon onClick={()=>setTheme('dark')} />
+            }
+        </div>
     </div>
   )
 }
