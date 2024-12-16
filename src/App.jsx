c096f072ce477547d79d69dfd24853895fc15c77
wacho91 <criskol.71@gmail.com>
2024-12-16 12:23:36 -0500

    Home Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 
 import { createContext, useEffect, useState } from "react"
 import Header from "./Components/Header"
+import Home from "./Pages/Home"
 import { ThemeContext } from "./Context/ThemeContext"
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +17,7 @@ function App() {
     <ThemeContext.Provider value={{theme,setTheme}}>
       <div className={`${theme} ${theme=='dark'?'bg-[#121212]':null}`}>
         <Header />
+        <Home />
       </div>
     </ThemeContext.Provider>
   )
